512776de92f96f82a039714fd3ce65522088dda0
aorps <aorbeandrews@gmail.com>
2024-01-11 20:56:05 -0500

    added .gitignore and reformat infra.tf
    Signed-off-by: aorps <aorbeandrews@gmail.com>

--- vultr/infra.tf
+++ vultr/infra.tf
@@ -1,19 +1,19 @@
 terraform {
   required_providers {
     vultr = {
-      source = "vultr/vultr"
+      source  = "vultr/vultr"
       version = "2.19.0"
     }
   }
 }
 
 provider "vultr" {
-  api_key = var.vultr_api_key
-  rate_limit = 100
+  api_key     = var.vultr_api_key
+  rate_limit  = 100
   retry_limit = 3
 }
 
 resource "vultr_instance" "inst" {
   region = var.location
-	plan = var.plan
+  plan   = var.plan
 }
